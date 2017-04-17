0628f360ca5b64cfa47417502f0485f439623498
Carlos Martinez <martinezcj2@gmail.com>
2017-04-17 01:35:21 -0400

    Add extract text plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
 
 module.exports = {
@@ -7,6 +8,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test:/\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+          publicPath: '/dist'
+        })
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       // favicon: 'add path to favicon',
@@ -14,7 +27,11 @@ module.exports = {
     minify: {
       collapseWhitespace: true
     },
-    hash: true,
     template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
-  })]
+  }),
+    new ExtractTextPlugin({
+      filename: 'App.css',
+      allChunks: true
+    })
+  ]
 }
