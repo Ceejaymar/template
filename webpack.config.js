983c2721d8aa524f7a504fb979344981eb7a01e3
Carlos Martinez <martinezcj2@gmail.com>
2017-04-16 23:23:01 -0400

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: './dist/app.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin({
+      // favicon: 'add path to favicon',
+    title: 'Template',
+    minify: {
+      collapseWhitespace: true
+    },
+    hash: true,
+    template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+  })]
 }
