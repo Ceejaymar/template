f605302f84f01f3bedecbb5d7a2618a3637ad3e4
Carlos Martinez <martinezcj2@gmail.com>
2017-04-18 00:22:30 -0400

    Add hot module reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
 const path = require('path');
+
+const isProd = process.env.NODE_ENV === 'production';
+const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+const cssProd = ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: ['css-loader', 'sass-loader'],
+                  publicPath: '/dist'
+                })
+
+const cssConfig = isProd ? cssProd : cssDev;
 
 module.exports = {
   entry: './src/App.js',
@@ -12,11 +23,7 @@ module.exports = {
     rules: [
       {
         test:/\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
-          publicPath: '/dist'
-        })
+        use: cssConfig
       },
       { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }
     ]
@@ -26,7 +33,8 @@ module.exports = {
     compress: true,
     port: 9000,
     stats: 'minimal',
-    open: true
+    open: true,
+    hot: true
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -40,7 +48,10 @@ module.exports = {
   }),
     new ExtractTextPlugin({
       filename: 'App.css',
+      disable: !isProd,
       allChunks: true
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
   ]
 }
